d711d67e9fee456c8883ea2f63cc969ab6421c5f
Chad Woolley & Josh Susser <thewoolleyman+josh_and_chad@gmail.com>
2009-05-14 16:04:40 -0700

    first pass at solr recipe

---
+++ cookbooks/solr/recipes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: solr
+# Recipe:: default
+#
+
+require 'digest/sha1'
+
+directory "/data/#{app}/jettyapps" do
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0755
+  recursive true
+end
+
+remote_file "/data/#{app}/jettyapps/solr" do
+  source "solr"
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0755
+end
+
+template "/etc/monit.d/solr.#{app}.monitrc" do
+  source "solr.monitrc.erb"
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0644
+  variables({
+    :app => app
+  })
+end
+
+execute "restart-monit-solr" do
+  command "/usr/bin/monit restart all -g solr_#{app}"
+  action :run
+end
