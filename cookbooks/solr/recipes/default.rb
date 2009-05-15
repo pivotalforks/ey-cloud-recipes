a71e1a6b547e0b97367ef834bf1d0c4bf947c318
Chad Woolley & Josh Susser <thewoolleyman+josh_and_chad@gmail.com>
2009-05-15 14:58:22 -0700

    add install of solr package from apache site

--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -7,7 +7,7 @@ require 'digest/sha1'
 
 node[:applications].each do |app,data|
 
-  directory "/data/#{app}/jettyapps/solr" do
+  directory "/data/#{app}/jettyapps" do
     owner node[:owner_name]
     group node[:owner_name]
     mode 0755
@@ -43,6 +43,13 @@ node[:applications].each do |app,data|
 
   execute "restart-monit-solr" do
     command "/usr/bin/monit restart all -g solr_#{app}"
+    action :run
+  end
+
+  execute "install solr package" do
+    command("cd /data/#{app}/jettyapps/ && " +
+            "wget -O apache-solr-1.3.0.tgz http://mirror.cc.columbia.edu/pub/software/apache/lucene/solr/1.3.0/apache-solr-1.3.0.tgz && " +
+            "tar --directory=solr -strip-components=1 -zxvf -O apache-solr-1.3.0.tgz")
     action :run
   end
 
